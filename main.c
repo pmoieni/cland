6c94268d8df04110ea1e6f8d78f2a1a11aed10d2
pmoieni <62774242+pmoieni@users.noreply.github.com>
2025-12-14 23:47:39 +0330

    format config

--- main.c
+++ main.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-
-int main() {
-  int i = 0, sum = 0, x;
-  do {
-    printf("enter a number:\n");
-    scanf("%d", &x);
-    i++;
-    sum += x;
-  } while (i < 10);
-
-  printf("avg is: %f", ((float)sum / i));
-  return 0;
+int main(int argc, char* argv[]) {
+    return 0;
 }
